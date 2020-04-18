77d034d03a3e13770d15707eb69947881938d18c
ekater1na <ekaterina_l@tut.by>
2020-04-18 18:41:42 +0300

    feat: report progress with setInterval

--- timer.js
+++ timer.js
@@ -1,17 +1,22 @@
-const waitTime = 3000;
+const waitTime = 5000;
 const waitInterval = 500;
 let currettTime = 0;
 
 const incTime = () => {
     currettTime += waitInterval;
-    console.log(`waiting ${currettTime / 1000 } seconds`);
+    const p = Math.floor((currettTime / waitTime) * 100);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`waiting ... ${p}%`)
 };
 
 console.log(`setting a ${waitTime / 1000} second delay`);
 
 const timerFinished = () => {
     clearInterval(interval);
-    console.log('done')
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    console.log('done');
 };
 
 const interval = setInterval(incTime, waitInterval);
